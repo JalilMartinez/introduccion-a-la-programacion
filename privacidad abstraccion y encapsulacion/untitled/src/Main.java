9294369636fa9a052c50fc6b8d9977bd3688557e
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-10-11 19:43:41 -0500

    a

--- privacidad abstraccion y encapsulacion/untitled/src/Main.java
+++ privacidad abstraccion y encapsulacion/untitled/src/Main.java
@@ -1,11 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         Vehiculo vehiculo= new Vehiculo();
+        Moto moto= new Moto();
         vehiculo.setTipo("Coche");
         System.out.println(vehiculo.getTipo());
     }
 }
 class Vehiculo{
+    private String tipo;
+
+    public void setTipo(String valor){
+        this.tipo= valor;
+    }
+    public String getTipo(){
+        return this.tipo;
+    }
+}
+
+abstract class Moto{
     private String tipo;
 
     public void setTipo(String valor){
